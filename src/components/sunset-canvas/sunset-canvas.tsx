f42cabb47b086dfb54fdff902fd6cc3229d353f3
Adam Jonsson <adamsmajl@gmail.com>
2021-04-21 21:28:09 +0200

    Remove unused import

--- src/components/sunset-canvas/sunset-canvas.tsx
+++ src/components/sunset-canvas/sunset-canvas.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useRef } from "react"
+import React, { FunctionComponent, useEffect, } from "react"
 import "./sunset-canvas.css";
 import Sky from "./sunset-elements/sky";
 import { SunsetTime } from "./sunset-elements/sunset-time";
